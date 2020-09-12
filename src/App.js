35b2e1563a7aeb99d65c40c04454c31c97ed7384
fercito22 <fercito22@hotmail.com>
2020-09-11 22:58:26 -0400

    Rutas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 import "./styles/styles.scss"
@@ -6,15 +6,31 @@ import Curso from './Curso'
 import Banner from "./Banner"
 import Formulario from "./Formulario"
 import CourseGrid from './CourseGrid';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 const App = () => (
-  <Fragment>    
-    
-   <Banner />
-   <Formulario name="Formulario EDTeam"/>
-   <CourseGrid />
+  // <Fragment>    
+  <Router>
+    <Switch>
 
-  </Fragment>
+      {/* <Banner />
+      <Formulario name="Formulario EDTeam"/>
+      <CourseGrid /> */}
+      <Route path="/" exact component={Banner} />
+      <Route path="/cursos" component={CourseGrid} />
+      <Route path="/cursos/ :id" component={Course} />
+      {/* coolback ()  */}
+      <Route path="/formulario" component={() => <Formulario name="Pagina de Contacto." />} />
+      <Route component={() => (
+        <div className="ed-grid">
+          <h1>Error 404</h1>
+          <span>Pagina no encontrada</span>
+        </div>
+      )} />
+
+    </Switch>
+  </Router>
+  //</Fragment>
 )
 //<h1>Hola Mundo desde mi archivo JS</h1>
 
